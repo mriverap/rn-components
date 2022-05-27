12389a77b33acdfea4296868ace49302c06f0aeb
Monica Rivera <mriverap@outlook.com>
2022-05-27 00:16:28 -0700

    Add Animation 2

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,10 +1,38 @@
-import {StyleSheet, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, View, Animated, PanResponder} from 'react-native';
+import React, {useRef} from 'react';
 
 const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x, // x,y are Animated.Value
+          dy: pan.y,
+        },
+      ],
+      {useNativeDriver: false},
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {
+          toValue: {x: 0, y: 0}, // Back to zero
+          useNativeDriver: false,
+        },
+      ).start();
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <View style={styles.centerBox} />
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.centerBox]}
+      />
     </View>
   );
 };
