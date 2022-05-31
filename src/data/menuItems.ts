834b0c4662d5520d92541b41f7cc3d4809e4f6c4
Monica Rivera <mriverap@outlook.com>
2022-05-30 17:07:37 -0700

    Add Slides Scree with fade in animation and conditional button

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -46,4 +46,9 @@ export const menuItems: MenuItem[] = [
     icon: 'download-outline',
     component: 'InfiniteScroll',
   },
+  {
+    name: 'Slides',
+    icon: 'flower-outline',
+    component: 'Slides',
+  },
 ];
--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -9,6 +9,7 @@ import PullToRefreshScreen from '../screens/PullToRefreshScreen';
 import SectionListScreen from '../screens/SectionListScreen';
 import ModalScreen from '../screens/ModalScreen';
 import InfiniteScrollScreen from '../screens/InfiniteScrollScreen';
+import SlidesScreen from '../screens/SlidesScreen';
 const Stack = createStackNavigator();
 
 import React from 'react';
@@ -26,6 +27,7 @@ const Navigator = () => {
       <Stack.Screen name="SectionList" component={SectionListScreen} />
       <Stack.Screen name="Modal" component={ModalScreen} />
       <Stack.Screen name="InfiniteScroll" component={InfiniteScrollScreen} />
+      <Stack.Screen name="Slides" component={SlidesScreen} />
     </Stack.Navigator>
   );
 };
